1236844d1373140a53412bbb68c79e4ef919f7e4
Unknown <nofar1407@gmail.com>
2019-01-07 23:49:28 +0200

    add comments

--- ex3.py
+++ ex3.py
@@ -9,8 +9,7 @@ import multiprocessing
 import math
 import pickle
 
-import csv
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 NUM_OF_CLUSTERS = 9
 LAMBDA = 0.05
@@ -40,11 +39,11 @@ def get_words_in_file(filename):
 
 
 def save_results_graph(data, datatype):
-    plt.figure()
-    plt.plot(range(len(data)), data)
-    plt.xlabel('epoch')
-    plt.ylabel(datatype)
-    plt.savefig(datatype + '.png')
+    # plt.figure()
+    # plt.plot(range(len(data)), data)
+    # plt.xlabel('epoch')
+    # plt.ylabel(datatype)
+    # plt.savefig(datatype + '.png')
 
 
 def get_articles_from_file(filename):
@@ -85,12 +84,14 @@ def remove_rare_words(wordsDictionary):
             if wordsDictionary[w] >= 4}
 
 
-class Article:
+class Article: # represent document in EM model
     def __init__(self, articleIndex, articleString, tags, vocabulary):
         articleWords = articleString.split()
         filteredWords = [word for word in articleWords if word in vocabulary]
 
+        # dictionary of words and number of times they appear in the article
         self.articleCounter = collections.Counter(filteredWords)
+
         self.length = len(filteredWords)
         self.tags = tags
 
@@ -111,7 +112,7 @@ class EMModel:
 
     def E_step(self):
         print("start E step")
-        for article in self.articles:
+        for article in self.articles: # calculate wt with underflow
             z = self.get_z(article)
             m = max(z)
 
@@ -133,8 +134,9 @@ class EMModel:
     def M_step(self):
         print("start M step")
 
-        self.set_alpha()
+        self.set_alpha() # updates alphas
 
+        # update P - split calculation to process for each cluster
         manager = multiprocessing.Manager()
         Ps = manager.dict()
         calc_Pi_processes = [multiprocessing.Process(target=self.calculate_pi, args=(i, Ps)) for i in
@@ -154,7 +156,7 @@ class EMModel:
 
         alpha_i = float(articleSumOfW) / len(self.articles)
 
-        return EPSILON if alpha_i == 0 else alpha_i
+        return EPSILON if alpha_i == 0 else alpha_i # change alpha to epsilon in case it equals to zero
 
     def set_alpha(self):
         alphas = [self.calculate_alpha_i(i) for i in range(NUM_OF_CLUSTERS)]
@@ -171,11 +173,11 @@ class EMModel:
                 enumerator_sum += article.wt[i] * article.articleCounter[k]
                 denumerator_sum += article.wt[i] * article.length
 
-            P_is[k] = (float(enumerator_sum) + LAMBDA) / (denumerator_sum + (len(self.vocabulary) * LAMBDA))
+            P_is[k] = (float(enumerator_sum) + LAMBDA) / (denumerator_sum + (len(self.vocabulary) * LAMBDA)) # smooting
 
         P[i] = P_is
 
-    def calculate_likelihood(self):
+    def calculate_likelihood(self): # calculate log likelihood with underflow
         articlesLikelihood = []
         for article in self.articles:
             z_is = self.get_z(article)
@@ -190,18 +192,20 @@ class EMModel:
         sumOfPerplexities = 0
 
         for article in self.articles:
-            prediction = np.argmax(article.wt)
+            prediction = np.argmax(article.wt) # article classified cluster
             sumOfWordsProbabilities = 0
+
             for k, n in article.articleCounter.items():
                 PofPerdictedCluster = (self.P[prediction][k] * article.length + LAMBDA) / \
-                                      (article.length + len(self.vocabulary) * LAMBDA)
+                                      (article.length + len(self.vocabulary) * LAMBDA) # probability of right classification
+
                 sumOfWordsProbabilities += np.log(PofPerdictedCluster) * n
 
             sumOfPerplexities += np.exp(sumOfWordsProbabilities / -article.length)
 
         return sumOfPerplexities / len(self.articles)
 
-    def create_confusion_matrix(self):
+    def create_confusion_matrix(self): # create confusion matrix for report
         matrix = []
         counter = np.zeros(NUM_OF_CLUSTERS)
 
@@ -220,25 +224,21 @@ class EMModel:
         fh = open("matrix.csv", "w")
         fh.write("\n".join(lines))
         fh.close()
-        # matrix = [[i+1].extend(row) for i, row in enumerate(matrix)]
-        # headers = ['index'].extend(self.topics)
-        # print(headers)
-        # with open('confusionMatrix.csv', 'w', newline='') as file:
-        #     writer = csv.DictWriter(file, fieldnames=headers)
-        #     writer.writeheader()
-        #     writer.writerows(matrix)
 
 
 if __name__ == "__main__":
     development_set_filename = sys.argv[1]
     topics_set_filename = sys.argv[2]
 
+    # get all words and tags from file
     developmentArticles, developmentArticlesTopics = get_articles_from_file(development_set_filename)
     developmentWords = get_all_words_in_articles(developmentArticles)
 
+    # count words and remove rare words (according to assignment instructions)
     wordsCounter = collections.Counter(developmentWords)
     wordsCounter = remove_rare_words(wordsCounter)
 
+    # get the list of all ords - used as vocabulary for the model
     vocabulary = wordsCounter.keys()
 
     topics = get_words_in_file(topics_set_filename)
@@ -250,16 +250,14 @@ if __name__ == "__main__":
 
     EM.M_step()
 
-    while True:  # change to threshold
+    while len(logLikelihoodArray) > 3 and logLikelihoodArray[-1] - logLikelihoodArray[-3] < STOP_CRITERIA:  # change to threshold
         print("epoch #" + str((len(logLikelihoodArray) + 1)))
 
-        if len(logLikelihoodArray) > 3 and logLikelihoodArray[-1] - logLikelihoodArray[-3] < STOP_CRITERIA:
-            print("Stop algorithm !")
-            break
-
+        # EM steps
         EM.E_step()
         EM.M_step()
 
+        # calculate perplexity and likelihood
         logLikelihood = EM.calculate_likelihood()
         perplexity = EM.calculate_mean_perplexity()
         print(logLikelihood)
@@ -267,6 +265,9 @@ if __name__ == "__main__":
         logLikelihoodArray.append(logLikelihood)
         meanPerplexityArray.append(perplexity)
 
+    print("Stop algorithm !")
+
+    # save results graph and excel
     save_results_graph(logLikelihoodArray, "likelihood")
     save_results_graph(meanPerplexityArray, "preplexity")
     EM.create_confusion_matrix()
